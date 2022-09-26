01daf77125bbeca7438e0ac01b35924240ebfc7a
Alexey Votentsev <votencevaleksejj@gmail.com>
2022-09-26 22:35:05 +0500

    restyling

--- js/index.js
+++ js/index.js
@@ -14,6 +14,19 @@ const swiperHero = new Swiper('.swiper__hero', {
     },
 });
 
+let angle = 0;
+const heroPolygon = document.querySelector('.hero__polygon-img');
+const heroNextBtn = document.querySelector('.swiper__hero-next');
+const heroPrevBtn = document.querySelector('.swiper__hero-prev');
+heroNextBtn.addEventListener('click', () => {
+    angle += 90;
+    heroPolygon.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+})
+heroPrevBtn.addEventListener('click', () => {
+    angle -= 90;
+    heroPolygon.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+})
+
 const swiperExclusive = new Swiper('.swiper__exclusive', {
     direction: 'horizontal',
     loop: true,
